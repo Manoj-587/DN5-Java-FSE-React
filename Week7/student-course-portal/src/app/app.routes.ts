852f9518a5698f76dafc842d7e246be6eba12818
Manoj-587 <mkumar07959@gmail.com>
2026-07-29 12:04:08 +0530

    completed Hands on 5

--- Week7/student-course-portal/src/app/app.routes.ts
+++ Week7/student-course-portal/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home';
 import { CourseListComponent } from './pages/course-list/course-list';
 import { StudentProfileComponent } from './pages/student-profile/student-profile';
 import { EnrollmentFormComponent } from './pages/enrollment-form/enrollment-form';
+import { ReactiveEnrollmentFormComponent } from './pages/reactive-enrollment-form/reactive-enrollment-form';
 
 export const routes: Routes = [
-  { path: '',        component: HomeComponent },
-  { path: 'courses', component: CourseListComponent },
-  { path: 'profile', component: StudentProfileComponent },
-  { path: 'enroll',  component: EnrollmentFormComponent },
-  { path: '**',      redirectTo: '' }
+  { path: '',               component: HomeComponent },
+  { path: 'courses',        component: CourseListComponent },
+  { path: 'profile',        component: StudentProfileComponent },
+  { path: 'enroll',         component: EnrollmentFormComponent },
+  { path: 'enroll-reactive', component: ReactiveEnrollmentFormComponent },
+  { path: '**',             redirectTo: '' }
 ];
